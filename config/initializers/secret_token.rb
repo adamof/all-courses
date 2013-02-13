d5e8e2d7583179f938532b145c1da4a52952bc70
badjo <stefan.adamof@gmail.com>
2013-02-13 20:59:46 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AllCourses::Application.config.secret_token = 'ae158db00b015e4ec2e6e210c2a55d27a148eb3aee41c65deba474fa1ce23a7e803acba88d0ef9a0f66a3c8eb2bc6d2b948c1d1b3a2002e085140bd5ab652bdd'
